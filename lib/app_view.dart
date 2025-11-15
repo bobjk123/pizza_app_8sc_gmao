1a05f454bd436acec1f6f2d4e62215bb78a72554
bobjk123 <aaron091039@hotmail.com>
2025-11-14 22:03:20 -0600

    Added login screens and blocs and app view

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app_8sc_gmao/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:pizza_app_8sc_gmao/screens/auth/views/welcome_screen.dart';
+import 'package:pizza_app_8sc_gmao/screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Pizza Delivery App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: ((context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return HomeScreen();
+          } else {
+            return WelcomeScreen();
+          }
+        }),
+      ),
     );
   }
 }
